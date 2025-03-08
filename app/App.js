dbfa1946e2cf1d534143321bc6fed667068f4f80
DR4GON\DeLong <gaming@delong-iu.de>
2025-03-08 16:55:40 +0100

    Add phone number input and confirmation buttons

--- app/App.js
+++ app/App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Eingegebene Handynummer:", phoneNumber);
+  };
+
   return (
     <View style={styles.container}>
       <Image
@@ -9,9 +16,20 @@ export default function App() {
         style={styles.logo}
         resizeMode="cover"
       />
-      <Text>
-        Open up App.js to start working on your app! by Sarah with 💕{" "}
-      </Text>
+      <Text>Gib deine Handynummer ein:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Handynummer"
+        keyboardType="phone-pad"
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+      />
+      <Text>Möchtest du mit dieser Nummer fortfahren?</Text>
+      <Text>Hinweise: Spielen ist nur mit Profil möglich.</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Ja" onPress={handleSubmit} />
+        <Button title="Nein" onPress={handleSubmit} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,8 +42,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+  },
   logo: {
     width: "100%",
     height: "80%",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    width: "100%",
+    paddingHorizontal: 10,
   },
 });
