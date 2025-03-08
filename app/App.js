f7918c398d99e6bd3e8d5a83ca4cfa784721e243
DR4GON\DeLong <gaming@delong-iu.de>
2025-03-08 17:04:06 +0100

    Adjust background color to logo

--- app/App.js
+++ app/App.js
@@ -41,6 +41,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#334158",
   },
   buttonContainer: {
     flexDirection: "row",
